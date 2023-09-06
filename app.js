e9ca95eacf924987ef2ea272710e683198b4446f
raushan011 <134772840+raushan011@users.noreply.github.com>
2023-09-06 22:30:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const inputsNodeList = document.querySelectorAll('.property input');
+
+function handleUpdate(){
+
+    const box = document.querySelector('.box');
+    const hOffset = document.getElementById('h-offset').value;
+    const vOffset = document.getElementById('v-offset').value;
+    const blurRadius = document.getElementById('blur').value;
+    const spread = document.getElementById('spread').value;
+    const color = document.getElementById('color').value;
+    const moz = document.querySelector('.moz');
+    const webkit = document.querySelector('.webkit');
+    const normal = document.querySelector('.normal');
+    
+    let boxShadowValue = box.style.boxShadow;
+    box.style.boxShadow = `${hOffset}px ${vOffset}px ${blurRadius}px ${spread}px ${color}`;
+
+    moz.textContent = `-moz-box-shadow: ${boxShadowValue}`;
+    webkit.textContent = `-webkit-box-shadow: ${boxShadowValue}`;
+    normal.textContent = `box-shadow: ${boxShadowValue}`;
+
+}
+
+
+inputsNodeList.forEach(input => {
+    input.addEventListener('change', handleUpdate);
+});
+inputsNodeList.forEach(input => {
+    input.addEventListener('mousemove', handleUpdate);
+});
+
+
+
